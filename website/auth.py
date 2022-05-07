dfd1863ef7a4d0e8d736bca7766c19dc6c227af2
MirhanTanriverdi <71703335+MirhanTanriverdi@users.noreply.github.com>
2022-05-07 22:16:36 +0200

    make it already

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,7 @@
-from functools import cache
-from flask import Blueprint, current_app, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
-from flask_login import current_user, login_user, logout_user, login_required, current_user
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
